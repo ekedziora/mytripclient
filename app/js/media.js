3a404825e2e55d71d318ec755b1b664e31b3bcae
ekedziora <ekedziora13@gmail.com>
2016-01-18 23:12:15 +0100

    Zaślepka dodawania filmów

--- app/js/media.js
+++ app/js/media.js
@@ -40,14 +40,40 @@ angular.module('media', [])
         };
     }])
 
-    .controller('VideoController', ['$scope', 'VideoService', function ($scope, videoService) {
-        //TODO
+    .controller('VideoController', ['$scope', 'VideoService', 'TripDataShare', function ($scope, videoService, tripDataShare) {
+        $scope.addVideo = function (videoFile) {
+            videoService.addVideo(
+                {
+                    id: 1,//TODO do usunięcia
+                    tripId: tripDataShare.tripId,
+                    file: videoFile
+                },
+                function (res) {
+                    console.log("Successfully added the file!");
+                    console.log(res);
+                },
+                function (res) {
+                    console.log("Failure adding the file!");
+                    console.log(res);
+                }
+            );
+        };
+
+        $scope.setFile = function(videoFile) {
+            $scope.videoFile = videoFile.files[0];
+        };
     }])
 
-    .service('VideoService', ['$scope', function ($scope) {
-        //TODO
+    .service('VideoService', ['$http', function ($http) {
+        var baseUrl = "http://mytrippwapi.azurewebsites.net/api/";
 
         return {
-            //TODO
+            addVideo: function (data, success, error) {
+                var fd = new FormData();
+                fd.append('file', data.file);
+
+                $http.post(baseUrl + 'Media/addMovie?id=' + data.id + '&tripId=' + data.tripId, fd,
+                    {headers: {'Content-Type': undefined}}).then(success, error);
+            }
         };
     }]);
